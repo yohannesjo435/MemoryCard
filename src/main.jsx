ce4d7c71356c372412972073ca8321af6e7d5a30
Yohannes Tesfaye <yohannes435@gmaill.com>
2025-02-14 12:35:06 -0900

    added deploy.yml

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import Test from './Components/test.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Test></Test>
+    <App />
   </StrictMode>,
 )
